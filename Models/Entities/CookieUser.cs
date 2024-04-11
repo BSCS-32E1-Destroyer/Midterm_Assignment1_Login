6c9d22756887dac3db9cee072f58c522b3a9d481
Warren Albino <warrenalbino277@gmail.com>
2024-04-12 03:26:07 +0800

    Add CookieUser.cs
    Add "Entities" subfolder under Model that contains the CookieUser.cs that stores Users

---
+++ Models/Entities/CookieUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm_Assignment1_Login.Models.Entities
+{
+    public class CookieUser
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+    }
+}
