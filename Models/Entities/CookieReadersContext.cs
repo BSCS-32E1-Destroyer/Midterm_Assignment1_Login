2791f8f7f6740f3587682b39cf1b0bb38ec6a212
Warren Albino <warrenalbino277@gmail.com>
2024-04-15 10:57:57 +0800

    Create CookieReadersContext.cs
    Add DbContext

---
+++ Models/Entities/CookieReadersContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Midterm_Assignment1_Login.Models.Entities
+{
+    public class CookieReadersContext : DbContext
+    {
+        public CookieReadersContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<CookieUser> Users { get; set; }
+    }
+}
