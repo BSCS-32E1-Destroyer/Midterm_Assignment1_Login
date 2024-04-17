cfa9d924bdaedac2989b3b9dfe8ba380a1259aeb
Warren Albino <warrenalbino277@gmail.com>
2024-04-18 00:58:17 +0800

    Create RegisterVm.cs
    Create Register View Model

---
+++ Models/ViewModels/RegisterVm.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm_Assignment1_Login.Models.ViewModels
+{
+    public class RegisterVm : LoginVm
+    {
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
