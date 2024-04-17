a30884e2b63d8da92d96736c343cb99ff901ae4d
Warren Albino <warrenalbino277@gmail.com>
2024-04-17 20:36:21 +0800

    Run Migrations
    -dotnet ef migrations add InitialCreate

--- Models/Entities/CookieReadersContext.cs
+++ Models/Entities/CookieReadersContext.cs
@@ -4,9 +4,10 @@ namespace Midterm_Assignment1_Login.Models.Entities
 {
     public class CookieReadersContext : DbContext
     {
-        public CookieReadersContext(DbContextOptions options) : base(options)
+        public CookieReadersContext(DbContextOptions<CookieReadersContext> options) : base(options)
         {
         }
+
 
         public DbSet<CookieUser> Users { get; set; }
     }
